23be6714c26421b3d5aa77f3c0e852a7ba2a9603
Havy Nguyen <havynguyen1228@yahoo.come>
2024-02-07 00:23:02 -0600

    i think I did somethings wrong

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,31 +15,43 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return 'Hello, ' + name + '!';
 }
+let name = 'Havy';
+console.log('Hello, ${name}!', helloName(name));
 // Remember to call the function to test
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
+return firstNumber + secondNumber;
 }
+let result = addNumbers(1, 2);
+console.log(result);
+
+//after I stuggled on number 2, i did number 3 with flying colors.
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(firstNumber, secondNumber, thirdNumber) {
+return firstNumber * secondNumber * thirdNumber;
 }
-
+let results = multiplyThree(2, 3, 4);
+console.log(results);
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+else if (number < 0) {
+  return false;
+    } 
 }
+console.log (isPositive(2));
+console.log (isPositive(-3)); 
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 
@@ -47,16 +59,25 @@ function isPositive(number) {
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+if (array.length ==0 ) {
+ return undefined; 
+} else {
+  return array[array.length - 1];
+   } 
 }
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
-
+for (let index = 0 < array.length; index++;) {
+  if (array[index] === value) {
+    return true;
+   } 
+ } 
+return false;
 }
-
+// I think I did something wrong my server crashed 
 // ----------------------
 // Stretch Goals
 // ----------------------
